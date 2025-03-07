7e599098dbd2cd38579460be1c55cbbf02dbae40
Spencer Davis <spencerdavis226@gmail.com>
2025-03-07 11:07:43 -0500

    Added cookie-parser  package, and explored cookies in browser

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // middleware
-app.use(express.static('public'));
+app.use(express.static('public')); // refers to folder named "public", making it available everywhere
 app.use(express.json()); // Takes JSON data that comes with requests, and parses into a JS object. Then attaches that object to request body (req.body).
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -23,3 +25,20 @@ mongoose
 app.get('/', (req, res) => res.render('home')); // Express looks for "home" within a "views" folder by default
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+// cookies
+app.get('/set-cookies', (req, res) => {
+  // res.setHeader('Set-Cookie', 'newUser=true'); // Built in cookie setter
+  res.cookie('newUser', false); // cook-parser package allows this(.cookie). Easier than above default way
+  res.cookie('isEmployee', true, {
+    maxAge: 1000 * 60 * 60 * 24, // math = 1 day in ms. Cookie lasts 1 day, instead of just when session ends (leave page)
+    httpOnly: true,
+  });
+
+  res.send('you got the cookies!');
+});
+app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies.newUser);
+  res.json(cookies);
+});
